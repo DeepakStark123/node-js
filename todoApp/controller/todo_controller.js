86099e11c0c32c487dcf8e1d5304e4ecaa61b653
Deepak Kumar <deepak.k@aphroecs.com>
2024-03-04 12:03:27 +0530

    todo controller/service class update

--- todoApp/controller/todo_controller.js
+++ todoApp/controller/todo_controller.js
@@ -2,46 +2,54 @@ const ToDoService = require("../services/todo_services");
 const UserService = require("../services/user_services");
 
 exports.createToDo = async (req, res, next) => {
-    try {
-        const { userId, title, desc } = req.body;
-        const isUserExist = await UserService.checkUserById(userId);
-        if (isUserExist == false) {
-            return res
-                .status(200)
-                .json({ status: false, error: `User with id ${userId} not exist` });
-        }
-        let todoData = await ToDoService.createToDo(userId, title, desc);
-        res.json({ status: true, success: todoData });
-    } catch (error) {
-        console.log("CreateToDo err---->", error);
-        next(error);
+  try {
+    const { userId, title, desc } = req.body;
+    const isUserExist = await UserService.checkUserById(userId);
+    if (isUserExist == false) {
+      return res
+        .status(200)
+        .json({ status: false, error: `User with id ${userId} not exist` });
     }
+    let todoData = await ToDoService.createToDo(userId, title, desc);
+    res.json({ status: true, success: todoData });
+  } catch (error) {
+    console.log("CreateToDo err---->", error);
+    next(error);
+  }
 };
 
 exports.getToDoList = async (req, res, next) => {
-    try {
-        const { userId } = req.body;
-        const isUserExist = await UserService.checkUserById(userId);
-        if (isUserExist == false) {
-            return res
-                .status(200)
-                .json({ status: false, error: `User with id ${userId} not exists` });
-        }
-        let todoData = await ToDoService.getUserToDoList(userId);
-        res.json({ status: true, success: todoData });
-    } catch (error) {
-        console.log("GetToDoList err---->", error);
-        next(error);
+  try {
+    const { userId } = req.body;
+    const isUserExist = await UserService.checkUserById(userId);
+    if (isUserExist == false) {
+      return res
+        .status(200)
+        .json({ status: false, error: `User with id ${userId} not exists` });
     }
+    let todoData = await ToDoService.getUserToDoList(userId);
+    res.json({ status: true, success: todoData });
+  } catch (error) {
+    console.log("GetToDoList err---->", error);
+    next(error);
+  }
 };
 
 exports.deleteToDo = async (req, res, next) => {
     try {
-        const { id } = req.body;
-        let deletedData = await ToDoService.deleteToDo(id);
-        res.json({ status: true, success: deletedData });
+      const { id } = req.body;
+      var itemExist = await ToDoService.checkToDoById(id);
+      console.log("ItemExist Exist ==>", itemExist);
+      if (!itemExist) {
+        console.log(`Item with id ${id} not exists`);
+        return res
+          .status(200)
+          .json({ status: false, error: `Item with id ${id} not exists` });
+      }
+      let deletedData = await ToDoService.deleteToDo(id);
+      res.json({ status: true, success: deletedData });
     } catch (error) {
-        console.log("DeleteToDo err---->", error);
-        next(error);
+      console.log("DeleteToDo Exceptions---->", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-};
+  };
--- todoApp/services/todo_services.js
+++ todoApp/services/todo_services.js
@@ -2,38 +2,38 @@ const { deleteToDo } = require("../controller/todo_controller");
 const ToDoModel = require("../models/todo_model");
 
 class ToDoService {
-     static async createToDo(userId, title, description) {
-          const createToDo = new ToDoModel({ userId, title, description });
-          return await createToDo.save();
-     }
+  static async createToDo(userId, title, description) {
+    const createToDo = new ToDoModel({ userId, title, description });
+    return await createToDo.save();
+  }
 
-     static async getUserToDoList(userId) {
-          const todoList = await ToDoModel.find({ userId });
-          return todoList;
-     }
+  static async getUserToDoList(userId) {
+    const todoList = await ToDoModel.find({ userId });
+    return todoList;
+  }
 
-     static async deleteToDo(id) {
-          const deleted = await ToDoModel.findByIdAndDelete({ _id: id });
-          return deleted;
-     }
+  static async deleteToDo(id) {
+    const deleted = await ToDoModel.findByIdAndDelete({ _id: id });
+    return deleted;
+  }
 
-     //----Check-User-Exist-Or-Not--ById--
-     static async checkToDoById(todoId) {
-          try {
-               const todo = await ToDoModel.findById(todoId);
-               // const user = await ToDoModel.findOne({ _id: todoId });
-               if (todo) {
-                    console.log("Item exists:", user);
-                    return true;
-               } else {
-                    console.log("Item does not exist");
-                    return false;
-               }
-          } catch (error) {
-               console.error("Error checking todo item ID:", error);
-               return false;
-          }
+  //----Check-User-Exist-Or-Not--ById--
+  static async checkToDoById(todoId) {
+    try {
+     // Find the to-do item by ID
+     const todo = await ToDoModel.findById(todoId);
+     if (todo) {
+         console.log("Item exists:", todo);
+         return true;
+     } else {
+         console.log("Item does not exist");
+         return false;
      }
+    } catch (error) {
+      console.error("Error checking todo item ID:", error);
+      return false;
+    }
+  }
 }
 
 module.exports = ToDoService;
